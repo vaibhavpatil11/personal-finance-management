64e0e26cd67846d5b3f11f72cbc0391265279b28
Aishwarya Patil <83339467+Aishwaryapatil114@users.noreply.github.com>
2024-07-22 12:04:01 +0530

    changes dir structure

--- notification/notification/src/main/java/com/pem/notification/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.pem.notification.service;
-
-import com.pem.notification.model.Notification;
-
-public interface EmailService {
-	
-	public boolean sendMessage(String to, String subject, Notification notification);
-
-}
